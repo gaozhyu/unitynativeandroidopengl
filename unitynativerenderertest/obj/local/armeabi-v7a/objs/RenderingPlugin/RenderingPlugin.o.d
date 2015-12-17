c1c7174a9cc54b675f5bd5888a3d4be3c4220d65
gaozhenyu <zhyu.gao@hotmail.com>
2015-12-17 14:47:31 +0800

    test

---
+++ unitynativerenderertest/obj/local/armeabi-v7a/objs/RenderingPlugin/RenderingPlugin.o.d
@@ -0,0 +1,150 @@
+obj/local/armeabi-v7a/objs/RenderingPlugin/RenderingPlugin.o: \
+ jni/RenderingPlugin.cpp jni/RenderingPlugin.h \
+ jni/Unity/IUnityInterface.h jni/Unity/IUnityGraphics.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/math.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/../include/math.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stddef.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdint.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/../include/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stdio.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/../include/stdio.h \
+ D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stdarg.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/jni.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/log.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/GLES2/gl2.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/GLES2/gl2platform.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/KHR/khrplatform.h
+
+jni/RenderingPlugin.h:
+
+jni/Unity/IUnityInterface.h:
+
+jni/Unity/IUnityGraphics.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/math.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/../include/math.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stddef.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/../include/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stdio.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/../include/stdio.h:
+
+D:/Tools/Android/android-ndk-r10b/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/jni.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/GLES2/gl2.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/GLES2/gl2platform.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-9/arch-arm/usr/include/KHR/khrplatform.h:
---
+++ unitynativerenderertest/obj/local/armeabi/objs/RenderingPlugin/RenderingPlugin.o.d
@@ -0,0 +1,90 @@
+obj/local/armeabi/objs/RenderingPlugin/RenderingPlugin.o: \
+ jni/RenderingPlugin.cpp jni/RenderingPlugin.h \
+ jni/Unity/IUnityInterface.h jni/Unity/IUnityGraphics.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/math.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/stdint.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/stdio.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h \
+ D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h
+
+jni/RenderingPlugin.h:
+
+jni/Unity/IUnityInterface.h:
+
+jni/Unity/IUnityGraphics.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/math.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/GLES2/gl2.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/GLES2/gl2platform.h:
+
+D:/Tools/Android/android-ndk-r10b/platforms/android-19/arch-arm/usr/include/KHR/khrplatform.h:
